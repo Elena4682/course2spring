df1ac855a4bfe34bc934075ab38f5f61e6a73072
Елена <simonaitene@yandex.ru>
2023-06-04 19:49:51 +0300

    ДЗ на проверку

---
+++ src/main/java/pro/sky/course2spring/CartService.java
@@ -0,0 +1,22 @@
+package pro.sky.course2spring;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.Collection;
+import java.util.Set;
+@Service
+@SessionScope
+public class CartService {
+    private final Set<Integer> items;
+    public CartService(Set<Integer> items){
+        this.items = items;
+    }
+    public void add(Set<Integer> ids){
+        items.addAll(ids);
+    }
+    public Set<Integer> get() {
+        return Collection.unmodifiableSet(items);
+    }
+
+}
